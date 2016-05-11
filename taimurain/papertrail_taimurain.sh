96b3489be73dcd2eb768db3cd746c2aed6e87325
kingshy <kingshy@sayhello.com>
2016-05-10 17:10:36 -0700

    use v0.16 for PT

--- taimurain/papertrail_taimurain.sh
+++ taimurain/papertrail_taimurain.sh
@@ -2,14 +2,14 @@
 
 # RUN ONCE
 
-VERSION=1.3
+VERSION=1.4
 
 TEMP_DIR="/tmp/papertrail-app"
 mkdir -p $TEMP_DIR/opt/papertrail
 mkdir -p $TEMP_DIR/etc/papertrail
 mkdir -p $TEMP_DIR/etc/init/
 
-wget --directory-prefix /tmp/ https://github.com/papertrail/remote_syslog2/releases/download/v0.17/remote_syslog_linux_amd64.tar.gz
+wget --directory-prefix /tmp/ https://github.com/papertrail/remote_syslog2/releases/download/v0.16/remote_syslog_linux_amd64.tar.gz
 tar -xf /tmp/remote_syslog_linux_amd64.tar.gz -C /tmp/
 mv /tmp/remote_syslog/* $TEMP_DIR/opt/papertrail/
 
@@ -25,7 +25,7 @@ pre-start exec /usr/bin/test -e /etc/papertrail/taimurain_logs.yml
 exec /opt/papertrail/remote_syslog -D  -c /etc/papertrail/taimurain_logs.yml >> /var/log/papertrail.log 2>&1
 EOF
 
-fpm --force -s dir -C $TEMP_DIR -t deb --name "papertrail_taimurain" --version $VERSION --config-files etc/papertrail .
+fpm --force -s dir -C $TEMP_DIR -t deb --name "papertrail_taimurain_16" --version $VERSION --config-files etc/papertrail .
 
 aws s3 cp papertrail*.deb s3://hello-deploy/pkg/papertrail/
 rm -rf $TEMP_DIR
